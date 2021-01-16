775a68b10e9d11d742aa5d9366a830896068af41
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-01-16 07:09:27 +0000

    :tada: first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,64 @@
+let fs = require('fs');
+
+module.exports = {
+    title: '🌱 My Knowledge Garden 🌱',
+    description: 'A wiki for the things I learn to reference in future.',
+
+    head: [
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/public/favicons/apple-touch-icon.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/public/favicons/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/public/favicons/favicon-16x16.png"}],
+        ['link', { rel: "shortcut icon", href: "/public/favicons/favicon.ico"}],
+        ['meta', { name: "theme-color", content: "#ffffff"}],
+      ],
+
+    themeConfig: {
+      lastUpdated: 'Last Updated',
+      searchPlaceholder: 'Press / to search',
+      smoothScroll: true,
+
+        nav: [
+          { text: '🤖', link: 'https://abstractentropy.com/', target:'_self' },
+          { text: '🐦', link: 'https://twitter.com/CryDeTaan', target:'_self' },
+          { text: '🐙', link: 'https://google.com', target:'_self' },
+        ],
+
+        
+        sidebar: [
+            {
+              collapsable: false,
+              children: pages('./docs/mkg/'),
+            },
+          ],
+
+
+      },
+      plugins: [
+        '@vuepress/back-to-top',
+        '@vuepress/nprogress'
+      ]
+
+}
+
+// https://stackoverflow.com/a/53271115
+
+function pages(path) {
+  let files = fs.readdirSync(path);
+  let list = ["/"];
+
+  for (let i in files) {
+      
+    let filename = files[i].split('.').slice(0, -1).join('.');
+
+    if (filename.toLowerCase() !=="readme") {
+
+        let filePath = path.split('/').slice(2, -1)[0];
+        list.push(`/${filePath}/${filename}`);
+
+    }
+    
+  }
+
+  return list;
+
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+# Introduction
+
+It seams my memory is at a rudimentary stage of evolution and trying to remember many things and for long periods of time is futile. 
+I am started this wiki and call it my knowledge garden where I make notes I learn or read which I can reference in future, albeit slower than my :brain:, its definitely more reliable. 
+
+::: tip INTENTION
+I intend to digest something every other day which I will then try to add to my knowledge garden :crossed_fingers:
+:::
+
+## Technicals - for those interested
+
+For my knowledge garden I use a minimally configure VuePress site, which I push to GitHub and let netlify take care of the rest.
+
+To add additional pages, it is as simple as adding a new `.md` file to the **mkg** directory. Add content using Markdown, see the VuePress [Markdown Extensions](https://vuepress.vuejs.org/guide/markdown.html) for more information, and use lots of these :tada: :art: :100:
